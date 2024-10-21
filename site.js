63643000f70b4ac51e6957312d5c653681b1f43a
Sebastian Poquette <500197548@fvtc.edu>
2024-10-21 14:48:43 -0500

    exam complete

--- site.js
+++ site.js
@@ -1,4 +1,3 @@
-
 // import the utility functions "decodeHtml" and "shuffle"
 import { decodeHtml, shuffle } from './utils.js' 
 
@@ -9,14 +8,47 @@ const nextQuestionElement = document.querySelector('#nextQuestion')
 
 // IIFE (so we can use async/await)
 ;(async () => {
+    
+    // Step 1: Create "getNextQuestion" function
+    const getNextQuestion = async () => {
+        const fetchResponse = await fetch('https://opentdb.com/api.php?amount=1&category=21&difficulty=easy&type=multiple')
+        const json = await fetchResponse.json()
+        
+        const { question, correct_answer: correct, incorrect_answers: incorrect } = json.results[0]
+        const answers = shuffle([ ...incorrect, correct ])
+        return { question, answers, correct }
+    }
 
-	// todo: create your "getNextQuestion" function
+    // Step 2: Create "renderQuestion" function
+    const renderQuestion = ({ question, answers, correct }) => {
 
-	// todo: create your "renderQuestion" function
+        questionElement.textContent = decodeHtml(question)
+        answersElement.innerHTML = ''
 
-	// todo: add the event listener to the "nextQuestion" button
+        answers.forEach(answer => {
+            const button = document.createElement('button')
+            button.textContent = decodeHtml(answer)
+            button.addEventListener('click', () => {
+                if (answer === correct) {
+                    button.classList.add('correct')
+                    answersElement.querySelectorAll('button').forEach(b => b.disabled = true)
+                    alert('Correct!')
+                } else {
+                    button.disabled = true
+                    alert('Incorrect!')
+                }
+            })
+            answersElement.appendChild(button)
+        })}
+
+    // Step 3: Add event listener to "nextQuestion" button
+    nextQuestionElement.addEventListener('click', async () => {
+        renderQuestion(await getNextQuestion())
+        nextQuestionElement.disabled = true
+        setTimeout(() => nextQuestionElement.disabled = false, 10000)
+    })
 
 })()
 
-// mimic a click on the "nextQuestion" button to show the first question
+// Automatically load the first question
 nextQuestionElement.click()
